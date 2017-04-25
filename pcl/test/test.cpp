41e886c05f1ba28094f3efddc922f5030f0d47ff
minaminoki <sunahukinn1353@me.com>
2017-04-25 17:16:42 +0900

    add pcl test

---
+++ pcl/test/test.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <pcl/io/pcd_io.h>
+#include <pcl/point_types.h>
+
+int main (int argc, char** argv)
+{
+  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZ>);
+  if (pcl::io::loadPCDFile<pcl::PointXYZ>("../src.pcd", *cloud) == -1) //* load the file
+  {
+    std::cout << "Couldn't read file src.pcd " << std::endl;
+    return -1;
+  }
+  std::cout << "Loaded "
+            << cloud->width * cloud->height
+            << " data points from test_pcd.pcd with the following fields: "
+            << std::endl;
+  for (size_t i = 0; i < cloud->points.size (); ++i)
+    std::cout << "    " << cloud->points[i].x
+              << " "    << cloud->points[i].y
+              << " "    << cloud->points[i].z << std::endl;
+
+  return 0;
+}
